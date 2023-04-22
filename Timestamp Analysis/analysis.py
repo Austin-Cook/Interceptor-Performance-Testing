a242a1fbc500dca94bb54eef7b8c77b87bce1848
Austin-Cook <austin.cook.id@gmail.com>
2023-04-22 13:26:44 -0600

    Complete most of performance testing

--- Timestamp Analysis/analysis.py
+++ Timestamp Analysis/analysis.py
@@ -117,7 +117,8 @@ def time_xor_encrypt(timestamp_list):
 		
 	print("Average time to perform an XOR: " + str(get_average(duration_list)))
 	
-	
+# BEG printed in osi_crypto_provider.c ln 523 after line "if (*one_time_pad == NULL || signal_buffer_len(*one_time_pad) < plaintext_len) {"
+# END printed in osi_crypto_provider.c ln 572 at end of if statement to extend one-time pad	
 def time_modify_one_time_pad(timestamp_list):
 	# Correctly order BEC and END when they have the same timestamp
 	reorder_ties_given_priority(timestamp_list, "BEG", "END")
@@ -222,11 +223,73 @@ def time_keystroke_to_destination_encrypted(timestamp_list):
 					print("Average time for 301-400: " + str(get_average(duration_list[300:400])))
 					print("Average time for 401-500: " + str(get_average(duration_list[400:500])))
 
-# BEG printed in osi_gui_encrypt.py function finish_encryption() ln 368
-# END printed in 
-def time_overlay_submit_plaintext_to_destination(timestamp_list):
+# OVERLAY - Time from when the user initiates decryption till plaintext appears in gui
+# REC printed in osi.c ln 586 after line "num_epoll_events = epoll_wait(osi->epoll_fd, epoll_events, NUM_EVENT_TYPES, -1);"
+# CFM printed in osi.c at beginning of function osi_perform_decryption_session()
+# END printed in osi_gui_base.py at end of method set_text()
+def time_overlay_decrypt_to_gui(timestamp_list):
+	# Correctly order REC and CFM when they have the same timestamp
+	reorder_ties_given_priority(timestamp_list, "REC", "CFM")			
 	
-			
+	duration_list = []
+	for stamp_index in range(len(timestamp_list)):
+		start_stamp = timestamp_list[stamp_index]
+		# if the stamp is a REC
+		if start_stamp.code == "REC":
+			if (stamp_index + 1) < len(timestamp_list):
+				# if the next one is CFM
+				if timestamp_list[stamp_index + 1].code == "CFM":
+					if (stamp_index + 2 < len(timestamp_list)):
+						# if the one after that is END
+						end_stamp = timestamp_list[stamp_index + 2]
+						if (end_stamp.code == "END"):
+							duration_list.append(get_time_difference(start_stamp, end_stamp))
+	
+	# Print useful info
+	print("timestamp_list length: " +str(len(timestamp_list)))
+	print("duration_list length: " + str(len(duration_list)))
+	for duration in duration_list:
+		print(str(duration))
+		
+	cfm_count = 0
+	end_count = 0
+	for i in range(len(timestamp_list)):
+		if timestamp_list[i].code == "CFM":
+			cfm_count += 1
+		if timestamp_list[i].code == "END":
+			end_count += 1
+	print("CFM count: " + str(cfm_count))
+	print("END count: " + str(end_count))
+	
+	print("Average time from ctrl+alt+u to plaintext in GUI: " + str(get_average(duration_list)))
+	
+# BEG printed in osi_gui_encrypt.py at beginning of function finish_encryption()
+# END printed in osi_kbd.c at end of function osi_kbd_force_paste()
+def time_overlay_encrypt_to_destination(timestamp_list):
+	# Correctly order BEC and END when they have the same timestamp
+	reorder_ties_given_priority(timestamp_list, "BEG", "END")
+
+	duration_list = []
+	for stamp_index in range(len(timestamp_list)):
+		if timestamp_list[stamp_index].code == "BEG":
+			if stamp_index < len(timestamp_list) - 1:
+				if timestamp_list[stamp_index + 1].code == "END":
+					# Ignore bug where BEG prints after END
+					duration_list.append(get_time_difference(timestamp_list[stamp_index], timestamp_list[stamp_index + 1]))
+
+	# Get the first 1000 elements
+	if len(duration_list) > 1000:
+		duration_list = duration_list[:1000]
+
+	# Print useful info
+	print("timestamp_list length: " + str(len(timestamp_list)))
+	for timestamp in timestamp_list:
+		print(timestamp.to_string())
+	print("duration_list length: " + str(len(duration_list)))
+	for duration in duration_list:
+		print(str(duration))
+		
+	print("Average time to create or update the one time pad: " + str(get_average(duration_list)))
 
 def main():
 	timestamp_list = None
@@ -243,8 +306,11 @@ def main():
 	#time_keystroke_to_plaintext_in_gui(timestamp_list)
 	#time_process_input_pre_encryption(timestamp_list)
 	#time_xor_encrypt(timestamp_list)
-	#time_modify_one_time_pad(timestamp_list)
-	time_keystroke_to_destination_encrypted(timestamp_list)
+	time_modify_one_time_pad(timestamp_list)
+	#time_keystroke_to_destination_encrypted(timestamp_list)
+	#time_overlay_decrypt_to_gui(timestamp_list)
+	#time_overlay_encrypt_to_destination(timestamp_list)
+	
 		
 	print("DONE")
 	
